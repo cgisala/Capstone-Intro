5633e60f8a6a6f80b4bb5b7c1672443e24951c8b
Chris <cgisala@msn.com>
2020-01-15 16:09:22 -0600

    added more comments

--- Lab1/Part3:_guess_the_number.py
+++ Lab1/Part3:_guess_the_number.py
@@ -11,14 +11,14 @@ guess = int(input("Guess a number between 1 and 10:  "))
 while(choice == 'y'):
     if guess == randomNum:
         print('Congratulation you guessed the number')
-        randomNum = random.randrange(1,10)
+        randomNum = random.randrange(1,10) #reinitialize random number
     elif guess < randomNum:
         print('Your guess is too low')
     else:
         print('Your guess is too high')
     
-    choice = input("Do you want to continue? y or n  ")
+    choice = input("Do you want to continue? y or n  ")  #reinitialize choice
     if choice == 'y':
-         guess = int(input("Guess a number between 1 and 10:  "))
+         guess = int(input("Guess a number between 1 and 10:  ")) #reinitialize guess
 
         
