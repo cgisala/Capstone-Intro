dc8755a9247e58151388d68dbbb518b51dc02bd8
Chris <cgisala@msn.com>
2020-01-15 15:58:47 -0600

    Lab 1

---
+++ Python warmup 1.py
@@ -0,0 +1,9 @@
+#Input
+name = input("What is your name?  ")
+birthMonth = input("What month were you born in?  ")
+nameLength = len(name)
+
+#Output
+print('hello' + name)
+print('Happy birthday month!' + birthMonth)
+print(f'There are {nameLength} letters in your name')
